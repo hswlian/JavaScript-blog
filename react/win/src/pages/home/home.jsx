6efa555f1952e2f184733dead0b42e8f72488c19
hswlian <hswlian@qq.com>
2018-07-04 17:42:49 +0800

    Revert "add"
    This reverts commit bd3a77aff5b2f5cd3a1ab59af44e4f7ad73aa816.

--- react/win/src/pages/home/home.jsx
+++ react/win/src/pages/home/home.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Router,Switch } from 'react-router-dom';
 import Header from '../../components/Header';
 import Menu from '../../components/menu';
-import MainLeft from '../../components/mainLeft';
+import TreeExample from '../../components/tree';
+import { Router,Switch } from 'react-router-dom';
 const DATA = {
     menuLeft : ['分屏模式','叫号','过号'],
     menuRight : ['帮助','退出','返回']
@@ -17,44 +17,22 @@ class Home extends Component{
                 <Header />
                 <Menu data={DATA}/>
                 <div className="g-main">
-                    <MainLeft />
+                    <div className="g-left">
+                        <div className="header"><span>一次分屏</span></div>
+                        <div className="search">
+                            <input className="ipt-search" placeholder="请输入关键字"/>
+                            <a className="btn btn-search">查询</a>
+                        </div>
+                        <div className="g-panel">
+                            <div className="option">
+                                <a>全选</a>
+                                <a>反选</a>
+                            </div>
+                            <TreeExample />
+                        </div>
+                    </div>
                     <div className="g-right">
                         <div className="header"><span>详情查看</span></div>
-                        <div className="container">
-                            <div className="liner">
-                                <div className="box">
-                                    <span>此处显示屏幕设置</span>
-                                </div>
-                                <div className="table">
-                                    <ul>
-                                        <li>
-                                            <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;logo图片：</span>
-                                            <a className="btn">更改图片</a>
-                                        </li>
-                                        <li>
-                                            <span>自定义字段1：</span>
-                                            <input placeholder="***医院"/>
-                                        </li>
-                                        <li>
-                                            <span>自定义字段2：</span>
-                                            <input placeholder="诊室医生"/>
-                                        </li>
-                                        <li>
-                                            <span>自定义字段3：</span>
-                                            <input placeholder="页"/>
-                                        </li>
-                                        <li>
-                                            <span>自定义字段4：</span>
-                                            <input placeholder="请到**诊室候诊"/>
-                                        </li>
-                                    </ul>
-                                    <div className="option">
-                                        <a className="btn">保存</a>
-                                        <a className="btn">放弃</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
--- react/win/src/pages/login/login.jsx
+++ react/win/src/pages/login/login.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Router,Switch } from 'react-router-dom';
 import history from '../../histroy/history';
-import {createStore} from 'redux';
 
 class Login extends Component{
     constructor(props){
@@ -31,6 +30,7 @@ class Login extends Component{
     render() {
         return(
             <div className="main">
+                <header><h2>医院显示屏</h2></header>
                 <div className="logo"><h1>信息显示屏系统</h1></div>
                 <div className="panel">
                     <h3>用户登录</h3>
